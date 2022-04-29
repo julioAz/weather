4340d709b5ed48468a119efc6ee3390baa2d19b6
Julio Azevedo <julio.azevedo@acct.global>
2022-04-28 21:37:25 -0300

    Ajuste readme

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState} from "react";
+import axios from "axios";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+
+  const [localtion, setLocation] = useState(false);
+  const [weather, setWeather] = useState(false);
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition((position)=> {
+      console.log('position latitude', position.coords.latitude,
+      'position longitude', position.coords.longitude);
+      getWeather('position latitude', position.coords.latitude,
+      'position longitude', position.coords.longitude);
+      setLocation(true)
+    })
+  }, 
+  [])
+
+  let getWeather = async (lat, long) => {
+    let response = await axios.get("http://api.openweathermap.org/data/2.5/forecast",
+    {
+      params: {
+        lat: lat,
+        lon: long,
+        appid: 'process.env.WEATHER_API_KEY',
+        lang: 'pt-br',
+        units: 'metric'
+      }
+    });
+    setWeather(response.data);
+    console.log(response.data);
+  }
+
+  if(localtion == false) {
+    return (
+      <>
+        <h2>Voce deve ativar a localização</h2>
+      </>
+    )
+  } else {
+    return (
+          <>
+              <h1>Weather in your city ({weather['weather'][0]['description']})</h1>
+              <ul>
+                  <li>Actual temperature: {[weather]['temp']}</li>
+                  <li>Max temperature: </li>
+                  <li>Min temperature: </li>
+                  <li>Pressure: </li>
+                  <li>Moisture: </li>
+              </ul>
+          </>
+      )
+    }
+  }
 
 export default App;
