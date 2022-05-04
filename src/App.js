290b46cdc5447afdd5c184353ad66541a03398dc
Julio Azevedo <julio.azevedo@acct.global>
2022-05-04 20:29:10 -0300

    Ajuste na API

--- src/App.js
+++ src/App.js
@@ -3,39 +3,41 @@ import axios from "axios";
 
 function App() {
 
-  const [localtion, setLocation] = useState(false);
-  const [weather, setWeather] = useState(false);
+  const [location, setLocation] = useState(false);
+  const [weather, setWeather] = useState();
 
   useEffect(() => {
     navigator.geolocation.getCurrentPosition((position)=> {
-      console.log('position latitude', position.coords.latitude,
-      'position longitude', position.coords.longitude);
-      getWeather('position latitude', position.coords.latitude,
-      'position longitude', position.coords.longitude);
+      getWeather(position.coords.latitude, position.coords.longitude);
       setLocation(true)
     })
   }, 
   [])
 
   let getWeather = async (lat, long) => {
-    let response = await axios.get("http://api.openweathermap.org/data/2.5/forecast",
+    let res = await axios.get("http://api.openweathermap.org/data/2.5/forecast",
     {
       params: {
         lat: lat,
         lon: long,
-        appid: 'process.env.WEATHER_API_KEY',
+        appid: '5c76eb05e1433598b5ec7f1fa1d92f6b',
         lang: 'pt-br',
         units: 'metric'
       }
     });
-    setWeather(response.data);
-    console.log(response.data);
+    setWeather(res.data);
   }
 
-  if(localtion == false) {
+  if(location === false) {
     return (
       <>
         <h2>Voce deve ativar a localização</h2>
+      </>
+    )
+  } else if (weather === false) {
+    return (
+      <>
+        <h2>Carregando...</h2>
       </>
     )
   } else {
@@ -43,7 +45,7 @@ function App() {
           <>
               <h1>Weather in your city ({weather['weather'][0]['description']})</h1>
               <ul>
-                  <li>Actual temperature: {[weather]['temp']}</li>
+                  <li>Actual temperature:</li>
                   <li>Max temperature: </li>
                   <li>Min temperature: </li>
                   <li>Pressure: </li>
